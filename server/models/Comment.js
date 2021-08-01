502425d5ad89bf737b55e1ccfa57205f48db2c36
김현민 <hyunmin@nextculture.kr>
2021-08-01 19:10:42 +0900

    댓글기능을 위한 스키마, 컴포넌트 생성

---
+++ server/models/Comment.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const commentSchema = mongoose.Schema(
+  {
+    writer: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    postId: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    reponseTo: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content: {
+      type: String,
+    },
+  },
+  { timestamp: true }
+) // 만든날짜 , updqte날짜가 표시됨
+
+const Comment = mongoose.model("comment", commentSchema)
+
+module.exports = { Comment }
